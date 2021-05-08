24b32c2a0a1668a44aa935ea5f7fd94daef29350
Shawn Presser <shawnpresser@gmail.com>
2021-05-07 19:57:39 -0500

    Rework the rest of the pytorch API tests

--- mtftorch/test/test_torch_api.py
+++ mtftorch/test/test_torch_api.py
@@ -44,8 +44,11 @@ def compare(f):
         torch.TensorMixin.ok = make_verifier(B)
         A1 = f(self, pytorch, *args, **kws, tensor=pytorch.tensor)
         B1 = f(self, torch, *args, **kws, tensor=torch.tensor)
+        assert len(A) == len(B)
+        assert len(A) > 0, "Ensure that .ok() is called at least once in every test"
         compare_return_values(self, A, B, f, *args, **kws)
-        compare_return_values(self, A1, B1, f, *args, **kws)
+        assert A1 is None and B1 is None, "Tests shouldn't return any results"
+        #compare_return_values(self, A1, B1, f, *args, **kws)
     return wrap
 
 class TestModule(TestCase):
@@ -63,7 +66,7 @@ class TestModule(TestCase):
     @compare
     def test_take(self, torch, tensor):
         src = tensor([[4, 3, 5],
-                      [6, 7, 8]])
+                      [6, 7, 8]]).ok()
         torch.take(src, tensor([0, 2, 5])).ok()
         src.take(tensor([0, 2, 5])).ok()
 
@@ -73,7 +76,7 @@ class TestModule(TestCase):
         a = tensor([[ 1.3398,  0.2663, -0.2686,  0.2450],
                     [-0.7401, -0.8805, -0.3402, -1.1936],
                     [ 0.4907, -1.3948, -1.0691, -0.3132],
-                    [-1.6092,  0.5419, -0.2993,  0.3195]])
+                    [-1.6092,  0.5419, -0.2993,  0.3195]]).ok()
         torch.argmax(a).ok()
         torch.argmax(a, dim=1).ok()
         a.argmax().ok()
@@ -85,13 +88,19 @@ class TestModule(TestCase):
         a = tensor([[-0.3841,  0.6320,  0.4254, -0.7384],
                     [-0.9644,  1.0131, -0.6549, -1.4279],
                     [-0.2951, -1.3350, -0.7694,  0.5600],
-                    [ 1.0842, -0.9580,  0.3623,  0.2343]])
-        return a, torch.mean(a), torch.mean(a, 1), torch.mean(a, 1, True)
+                    [ 1.0842, -0.9580,  0.3623,  0.2343]]).ok()
+        torch.mean(a).ok()
+        torch.mean(a, 1).ok()
+        torch.mean(a, 1, True).ok()
+        a.mean().ok()
+        a.mean(1).ok()
+        a.mean(1, True).ok()
 
     @compare
     def test_min(self, torch, tensor):
-        a = tensor([[ 0.6750,  1.0857,  1.7197]])
-        return a, torch.min(a)
+        a = tensor([[ 0.6750,  1.0857,  1.7197]]).ok()
+        torch.min(a).ok()
+        a.min().ok()
 
     @compare
     def test_min_dim(self, torch, tensor):
@@ -99,14 +108,20 @@ class TestModule(TestCase):
         a = tensor([[-0.6248,  1.1334, -1.1899, -0.2803],
                     [-1.4644, -0.2635, -0.3651,  0.6134],
                     [ 0.2457,  0.0384,  1.0128,  0.7015],
-                    [-0.1153,  2.9849,  2.1458,  0.5788]])
-        return a, torch.min(a, 1)
+                    [-0.1153,  2.9849,  2.1458,  0.5788]]).ok()
+        x = torch.min(a, 1)
+        x.indices.ok()
+        x.values.ok()
+        x = a.min(1)
+        x.indices.ok()
+        x.values.ok()
 
     @compare
     def test_minimum(self, torch, tensor):
-        a = torch.tensor((1, 2, -1))
-        b = torch.tensor((3, 0, 4))
-        return a, b, torch.minimum(a, b)
+        a = tensor((1, 2, -1)).ok()
+        b = tensor((3, 0, 4)).ok()
+        torch.minimum(a, b).ok()
+        a.minimum(b).ok()
 
     @compare
     def test_amin(self, torch, tensor):
@@ -114,22 +129,24 @@ class TestModule(TestCase):
         a = tensor([[ 0.6451, -0.4866,  0.2987, -1.3312],
                     [-0.5744,  1.2980,  1.8397, -0.2713],
                     [ 0.9128,  0.9214, -1.7268, -0.2995],
-                    [ 0.9023,  0.4853,  0.9075, -1.6165]])
-        return a, torch.amin(a, 1)
+                    [ 0.9023,  0.4853,  0.9075, -1.6165]]).ok()
+        torch.amin(a, 1).ok()
+        a.amin(1).ok()
 
     @compare
     def test_mm(self, torch, tensor):
         # >>> mat1 = torch.randn(2, 3)
         mat1 = tensor([[-0.6552,  0.0376,  0.8117],
-                       [-2.2243,  0.0596,  1.2111]])
+                       [-2.2243,  0.0596,  1.2111]]).ok()
         # >>> mat2 = torch.randn(3, 3)
         mat2 = tensor([[ 1.4362,  0.0861, -0.6185],
                        [ 1.4264,  2.3783,  0.3660],
-                       [-1.2079,  0.1771,  0.6216]])
+                       [-1.2079,  0.1771,  0.6216]]).ok()
         # >>> torch.mm(mat1, mat2)
         # tensor([[-1.8678,  0.1768,  0.9235],
         #         [-4.5724,  0.1647,  2.1504]])
-        return mat1, mat2, torch.mm(mat1, mat2)
+        torch.mm(mat1, mat2).ok()
+        mat1.mm(mat2).ok()
 
 
 
